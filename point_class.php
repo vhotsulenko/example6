e14a1adc273e2107ce8b5585798ea6bbc2b45a1a
vhotsulenko <paraplan@ukr.net>
2021-11-16 19:06:52 +0200

    first commit

--- point_class.php
+++ point_class.php
@@ -1,13 +1,11 @@
 <?php
-//include 'Ipoint.php';
 if (!defined('Point::NAME'))
 {
-class Point //implements Ipoint
-{
+class Point 
+    {
     const NAME = 'point';
     public $x;
     public $y;
     public $z;
-      
-}
+    }
 }
