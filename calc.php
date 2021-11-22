0abf041b56faf6163217560b73a24d278ed9eb26
vhotsulenko <paraplan@ukr.net>
2021-11-22 22:26:51 +0200

    sp

--- calc.php
+++ calc.php
@@ -1,12 +1,12 @@
 <?php
 require 'point_class.php';
 $point1 = new Point();
-$xa = $point1->x=4;
-$ya = $point1->y=5;
-$za = $point1->z=6;
+$xa = $point1->x = 4;
+$ya = $point1->y = 5;
+$za = $point1->z = 6;
 $point2 = clone $point1;
-$xb = $point2->x=7;
-$yb = $point2->y=8;
-$zb = $point2->z=9;
+$xb = $point2->x = 7;
+$yb = $point2->y = 8;
+$zb = $point2->z = 9;
 echo "Расстояние между точками A($xa,$ya,$za) и В($xb,$yb,$zb): ";
 echo sqrt(pow(($xb  - $xa),2) + pow(($yb - $ya),2) + pow(($zb - $za),2));
